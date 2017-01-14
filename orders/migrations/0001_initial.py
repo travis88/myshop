3cdc4d6041f595a0bb6f0e4b40d28a330f646670
travis88 <hellsballs99@gmail.com>
2017-01-14 19:45:57 +0300

    Orders add

---
+++ orders/migrations/0001_initial.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('shop', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Order',
+            fields=[
+                ('id', models.AutoField(serialize=False, verbose_name='ID', primary_key=True, auto_created=True)),
+                ('first_name', models.CharField(max_length=50)),
+                ('last_name', models.CharField(max_length=50)),
+                ('email', models.EmailField(max_length=254)),
+                ('address', models.CharField(max_length=250)),
+                ('postal_code', models.CharField(max_length=20)),
+                ('city', models.CharField(max_length=100)),
+                ('created', models.DateTimeField(auto_now_add=True)),
+                ('updated', models.DateTimeField(auto_now=True)),
+                ('paid', models.BooleanField(default=False)),
+            ],
+            options={
+                'ordering': ('-created',),
+            },
+        ),
+        migrations.CreateModel(
+            name='OrderItem',
+            fields=[
+                ('id', models.AutoField(serialize=False, verbose_name='ID', primary_key=True, auto_created=True)),
+                ('price', models.DecimalField(decimal_places=2, max_digits=10)),
+                ('quantity', models.PositiveIntegerField(default=1)),
+                ('order', models.ForeignKey(to='orders.Order', related_name='items')),
+                ('product', models.ForeignKey(to='shop.Product', related_name='order_items')),
+            ],
+        ),
+    ]
